b1803addc2da9c4a476b35782d261c1b4c7b1dd2
vivektheexpert1234 <93033518+vivektheexpert1234@users.noreply.github.com>
2021-12-11 10:56:25 +0800

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,21 @@
+canvas=document.getElementById("myCanvas");
+ctx=canvas.getContext("2d")
+color="cyan";
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.linewidth=2;
+ctx.arc(200,200,40,0,2*Math.PI);
+ctx.stroke();
+canvas.addEventListener("mousedown",my_mousedown);
+function my_mousedown(e)
+{
+    color=document.getElementById("color").value;
+    mouse_x=e.clientX-canvas.offsetLeft;
+    mouse_y=e.clientY-canvas.offsetTop;
+    console.log("x="+mouse_x+",Y="+mouse_y)
+    ctx.beginPath();
+    ctx.strokeStyle=color;
+    ctx.linewidth=2;
+    ctx.arc(mouse_x,mouse_y,40,0,2*Math.PI);
+    ctx.stroke();
+}
